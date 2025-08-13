e6e721354cb96ef333a17560c75b33b9f9461b9c
Vadym <VadimCorporateUkr@gmail.com>
2025-08-13 11:52:04 +0300

    NO JIRA - Add new logo fix home page stile

--- src/components/Header.js
+++ src/components/Header.js
@@ -11,20 +11,8 @@ export default function Header() {
         <div className="animated-logo" />
         <span className="site-name">CloudOpsMaster</span>
       </div>
-
-      <div className="header-center">
-          <a
-              href="https://docs.google.com/forms/d/e/1FAIpQLScnvM_iWlMPqTBiFwoU0DBkFL1WRy4V2ll6EIRv6S_ICOe00A/viewform?usp=header"
-              target="_blank"
-              rel="noreferrer"
-              className="header-link"
-            >
-              📩 Request Consultation
-           </a>
-      </div>
       <nav className="nav-menu">
         <Link to="/" className={location.pathname === "/" ? "active" : ""}>Home</Link>
-        <Link to="/portfolio" className={location.pathname === "/portfolio" ? "active" : ""}>Portfolio</Link>
         <Link to="/quest" className={location.pathname === "/quest" ? "active" : ""}>Quest</Link>
       </nav>
     </header>
